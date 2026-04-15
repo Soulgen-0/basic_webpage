6af628bf87e0d36a541509d11d1a357e403b88af
Soulgen-0 <BD2852000@gmail.com>
2026-04-15 13:57:11 -0400

    Added a style sheet to the website

--- dist/pages/form.js
+++ dist/pages/form.js
@@ -1,18 +1,27 @@
-const element = document.getElementById("form");
+const formElement = document.getElementById("form");
 function handleSubmit(event) {
     const form = event.currentTarget;
-    const resultParagraph = document.querySelector("#form-result");
-    if (!resultParagraph) {
-        console.log("Could not find the 'form-result' paragraph element.");
+    const nameParagraph = document.querySelector("#form-result");
+    const favLangParagraph = document.querySelector("#fav-lang-result");
+    if (!nameParagraph) {
+        console.log("Could not find the 'form-result paragraph element.");
+        return;
+    }
+    if (!favLangParagraph) {
+        console.log("Could not find the 'fav-lang-result' paragraph element.");
         return;
     }
     const data = new FormData(form);
-    resultParagraph.innerHTML = "You submitted: " + data.get("name_field");
+    const fav_lang = data.get("fav_lang");
+    const name = data.get("name_field");
+    nameParagraph.innerHTML = "You name is: " + name;
+    favLangParagraph.innerHTML = "Your favorite language is: " + fav_lang;
     event.preventDefault();
 }
-if (element instanceof HTMLFormElement) {
-    const form = element;
+if (formElement instanceof HTMLFormElement) {
+    const form = formElement;
     form.addEventListener("submit", handleSubmit);
+    console.log("Successfully listening..");
 }
 else {
     console.log("Could not find the form element.");
--- src/pages/form.ts
+++ src/pages/form.ts
@@ -1,22 +1,30 @@
-const element: HTMLElement | null = document.getElementById("form");
+const formElement: HTMLElement | null = document.getElementById("form");
 
 function handleSubmit(event: SubmitEvent) {
     const form = event.currentTarget as HTMLFormElement
-    const resultParagraph: HTMLParagraphElement | null = document.querySelector("#form-result")
-    if (!resultParagraph) {
-        console.log("Could not find the 'form-result' paragraph element.")
+    const nameParagraph: HTMLParagraphElement | null = document.querySelector("#form-result")
+    const favLangParagraph: HTMLParagraphElement | null = document.querySelector("#fav-lang-result")
+    if (!nameParagraph) {
+        console.log("Could not find the 'form-result paragraph element.")
+        return
+    }
+    if (!favLangParagraph) {
+        console.log("Could not find the 'fav-lang-result' paragraph element.")
         return
     }
 
     const data = new FormData(form)
-    resultParagraph.innerHTML = "You submitted: " + data.get("name_field")
+    const fav_lang = data.get("fav_lang") as string
+    const name = data.get("name_field") as string
+    nameParagraph.innerHTML = "You name is: " + name
+    favLangParagraph.innerHTML = "Your favorite language is: " + fav_lang
     event.preventDefault()
 }
 
-if (element instanceof HTMLFormElement) {
-    const form: HTMLFormElement = element
+if (formElement instanceof HTMLFormElement) {
+    const form: HTMLFormElement = formElement
     form.addEventListener("submit", handleSubmit)
-    console.log("Successfuly listening..")
+    console.log("Successfully listening..")
 } else {
     console.log("Could not find the form element.")
 }
